9f5b9f37a54b23a23f8f42379bdec3936d6f3fe9
Justin <justin@ubuntu.(none)>
2013-07-20 16:07:47 -0500

    added readme files and determining what type of orm or database to setup and use

---
+++ models/database/api_adapters/sqlite.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 20, 2013
+
+@author: justin
+'''
+
+class GoogleFinance(object):
+    '''
+    classdocs
+    '''
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print 'Inside GoogleFinance'
+        

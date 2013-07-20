eb193e16d15267dec0b6475053056f3e203b801b
Justin <justin@ubuntu.(none)>
2013-07-20 15:17:15 -0500

    created testing framework and cases

---
+++ models/models.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 20, 2013
+
+@author: justin
+'''
+
+class Models(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        print 'Inside Models'
+        
---
+++ strategy/logic/logic_tests.py
@@ -0,0 +1,27 @@
+'''
+Created on Jul 20, 2013
+
+@author: justin
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        print 'Running logic tests'
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
